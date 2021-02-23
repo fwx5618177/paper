8d80ae3ea0f1c369c465617039720271f35dd896
guagua <627856466@qq.com>
2021-02-24 01:04:23 +0800

    工具

---
+++ 自动备份保存.bat
@@ -0,0 +1,13 @@
+@echo off && setlocal enabledelayedexpansion
+
+git status
+pause
+set /p "strinfo=这次都修改了什么呀，随便写点什么："
+git add .
+git commit -am "%strinfo%"
+git status
+git switch master
+git merge dev
+git push origin master
+echo 保存完成
+git switch dev
